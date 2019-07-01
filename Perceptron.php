31d330067410a32218e23412f835ab018b91f18a
pavlix <pavel.kraus01@gmail.com>
2019-07-01 22:57:21 +0200

    guess function

--- Perceptron.php
+++ Perceptron.php
@@ -28,6 +28,52 @@ class Perceptron
         }
     }
 
+    public function guess(array $xVector): int
+    {
+        //normalize array keys
+        $xVector = array_values($xVector);
 
+        //check, if input vector is ok
+        $this->validateVector($xVector);
+
+        $sum = 0;
+        foreach ($xVector as $key => $value) {
+            $sum += $value * $this->weights[$key];
+        }
+
+        return $this->sign($sum);
+    }
+
+
+    /**
+     * Returns 1 if float is bigger than zero, -1 otherwise
+     *
+     * @param float $value
+     * @return int
+     */
+    private function sign(float $value): int
+    {
+        if($value > 0) {
+            return 1;
+        }
+
+        return -1;
+    }
+
+
+    private function validateVector(array $xVector): void
+    {
+        // check, if vector has right length
+        if(!count($xVector) === $this->length) {
+            throw new InvalidArgumentException('Expected length of vector is ' . $this->length);
+        }
+
+        // check, if type of values in vector is correct
+        foreach ($xVector as $value) {
+            if(!in_array(gettype($value), ['integer', 'double'])) {
+                throw new InvalidArgumentException('All values in vector has to bee numerical');
+            }
+        }
+    }
 
 }
--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 require_once 'Perceptron.php';
 
 $perceptron = new Perceptron(2 );
+
+var_dump($perceptron->guess([1, 2]));
